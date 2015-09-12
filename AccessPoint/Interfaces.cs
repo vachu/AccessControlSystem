a639c502c5126fe676d0cb636b9de9e78f7bca65
Srivathsan Madhavan <m.srivathsan@gmail.com>
2015-09-12 13:05:47 +0530

    First functional commit

---
+++ AccessPoint/Interfaces.cs
@@ -0,0 +1,55 @@
+﻿namespace Crossover
+{
+	/// <summary>
+	/// For the use of the Department Manager Client app
+	/// </summary>
+	public interface IManager
+	{
+	}
+
+	/// <summary>
+	/// For the use by Administrator / Configurator app, of the whole Access Control System
+	/// </summary>
+	public interface IAdmin
+	{
+	}
+
+	/// <summary>
+	/// Main for use by the Security / Control-room Personnel App
+	/// </summary>
+	public interface IMonitorControl
+	{
+	}
+
+	/// <summary>
+	/// For "pushing" messages / events to other AccessPoint component.  A response is not expected
+	/// </summary>
+	public interface IPush
+	{
+	}
+
+	/// <summary>
+	/// For "pulling" information from other AccessPoint component. A response is very much expected here
+	/// </summary>
+	public interface IPull
+	{
+	}
+
+	/// <summary>
+	/// All AccessPoint components implement this interface
+	/// </summary>
+	public interface IAccessPoint : IManager, IAdmin, IMonitorControl, IPull, IPush
+	{
+
+	}
+
+	/// <summary>
+	/// This interface must be implemented by the "hosting" app / module - could be
+	/// a simple Console App or a Service
+	/// </summary>
+	public interface IAccessPointContainer
+	{
+		IAccessPoint GetAccessPoint(string accPtID);
+		string[] GetAccessPointIDs();
+	}
+}
--- AccessPointContainer/Program.cs
+++ AccessPointContainer/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using Crossover;
+using System.Collections.Generic;
 
 namespace AccessPointContainer
 {
-	class MainClass
+	class MainClass : IAccessPointContainer
 	{
+		private const string CONTAINER_NAME = "ws://company/access_control_system/demo";
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			string[] accPtIDs = {
+				"/Site1/Dept_1/Bldg_1",
+				"/Site1/Dept_1/Bldg_2",
+				"/Site1/Dept_2/Bldg_1",
+				"/Site1/Dept_2/Bldg_2",
+				"/Site1/Dept_3/Bldg_1",
+				"/Site1/Dept_3/Bldg_2",
+				"ws://company-intranet-url/Site3/Dept_4/Bldg_3",
+					// AccessPoint in a different site / Container
+					// There could be multiple AccessPoint Containers in a single site
+			};
+			var theApp = new MainClass ();
+
+			foreach (var id in accPtIDs) {
+				s_accPtMap [id] = AccessPoint.Create (theApp, id);
+			}
+			Console.WriteLine ("Access Point Container is now running @ {0}\n\n", CONTAINER_NAME);
+
+			Console.Write ("Press the buzzword to quit: ");
+			var input = Console.ReadLine ();
+			while (string.Compare (input.Trim (), "shutdown", true) != 0) {
+				input = Console.ReadLine ();
+			} // Longevity loop
 		}
+
+		#region IAccessPointContainer implementation
+
+		private static Dictionary<string, IAccessPoint> s_accPtMap =
+			new Dictionary<string, IAccessPoint> ();
+
+		public IAccessPoint GetAccessPoint (string accPtID)
+		{
+			if (string.IsNullOrWhiteSpace (accPtID) ||
+				!s_accPtMap.ContainsKey(accPtID.Trim()))
+			{
+				return null;
+			}
+			return s_accPtMap [accPtID.Trim ()];
+		}
+
+
+		public string[] GetAccessPointIDs ()
+		{
+			string[] IDs = new string[s_accPtMap.Keys.Count];
+			s_accPtMap.Keys.CopyTo (IDs, 0);
+			return IDs;
+		}
+		#endregion
 	}
 }
